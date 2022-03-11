16ba513b8c0bfc5503fb9b1a9e62820d2ee577b4
Miguel Mendoza <36460814+MrMendoza-98@users.noreply.github.com>
2022-03-11 11:27:51 -0500

    general review

--- index.php
+++ index.php
@@ -1,13 +1,13 @@
 <?php 
 function xss_whitelist($input, $limit = null, $offser = 0){
-    $x = (string) $input;
+    $x = (string) $input; #Force input to be a string
 
-    $x = preg_replace("/[^a-zA-Z0-9 -:,.!?\/|]/", "",$x);
+    $x = preg_replace("/[^a-zA-Z0-9 -:,.!?\/|]/", "",$x); # Allow alphanumeric characters, whitespace and specific characters
 
-    if ($limit) {
+    if ($limit) { #Limit characters
         $x = substr($x, $offset, $limit);
     }
 
-    return htmlentities($x, ENT_QUOTES, 'UTF-8');
+    return htmlentities($x, ENT_QUOTES, 'UTF-8'); # Convert characters to HTML entities and return the clean string
 }
 ?>
